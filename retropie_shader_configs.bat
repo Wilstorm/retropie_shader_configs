5c89ba00667f7509958a41f3f8675ccad16c44dc
Wilstorm <caw269@yahoo.com>
2021-05-14 17:33:35 -0500

    Updated batch file with argparse parameters
    Updated batch file to reflect the argparse changes to the code.

--- retropie_shader_configs.bat
+++ retropie_shader_configs.bat
@@ -1,67 +1,67 @@
 @echo off
 
-python retropie_shader_configs.py mame2000 crtpi 1920 1080
-python retropie_shader_configs.py mame2003 crtpi 1920 1080
-python retropie_shader_configs.py 2003plus crtpi 1920 1080
-python retropie_shader_configs.py mame2010 crtpi 1920 1080
-python retropie_shader_configs.py mame2015 crtpi 1920 1080
-python retropie_shader_configs.py mame2016 crtpi 1920 1080
-python retropie_shader_configs.py fba2012 crtpi 1920 1080
-python retropie_shader_configs.py fbneo crtpi 1920 1080
-python retropie_shader_configs.py consoles crtpi 1920 1080
+python retropie_shader_configs.py mame2000 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2003 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py 2003plus -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2010 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2015 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2016 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py fba2012 -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py fbneo -s crtpi -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py consoles -s crtpi -c false -x 1920 -y 1080 -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('crtpi',False,1920,1080)"
 
-python retropie_shader_configs.py mame2000 crtpi 1280 720
-python retropie_shader_configs.py mame2003 crtpi 1280 720
-python retropie_shader_configs.py 2003plus crtpi 1280 720
-python retropie_shader_configs.py mame2010 crtpi 1280 720
-python retropie_shader_configs.py mame2015 crtpi 1280 720
-python retropie_shader_configs.py mame2016 crtpi 1280 720
-python retropie_shader_configs.py fba2012 crtpi 1280 720
-python retropie_shader_configs.py fbneo crtpi 1280 720
-python retropie_shader_configs.py consoles crtpi 1280 720
+python retropie_shader_configs.py mame2000 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2003 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py 2003plus -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2010 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2015 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2016 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py fba2012 -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py fbneo crtpi -s crtpi -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py consoles crtpi -s crtpi -c false -x 1280 -y 720 -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('crtpi',False,1280,720)"
 
-python retropie_shader_configs.py mame2000 crtpi curvature
-python retropie_shader_configs.py mame2003 crtpi curvature
-python retropie_shader_configs.py 2003plus crtpi curvature
-python retropie_shader_configs.py mame2010 crtpi curvature
-python retropie_shader_configs.py mame2015 crtpi curvature
-python retropie_shader_configs.py mame2016 crtpi curvature
-python retropie_shader_configs.py fba2012 crtpi curvature
-python retropie_shader_configs.py fbneo crtpi curvature
+python retropie_shader_configs.py mame2000 -s crtpi -c true -o all
+python retropie_shader_configs.py mame2003 -s crtpi -c true -o all
+python retropie_shader_configs.py 2003plus -s crtpi -c true -o all
+python retropie_shader_configs.py mame2010 -s crtpi -c true -o all
+python retropie_shader_configs.py mame2015 -s crtpi -c true -o all
+python retropie_shader_configs.py mame2016 -s crtpi -c true -o all
+python retropie_shader_configs.py fba2012 -s crtpi -c true -o all
+python retropie_shader_configs.py fbneo -s crtpi -c true -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('crtpi',True)"
 
-python retropie_shader_configs.py mame2000 zfast 1920 1080
-python retropie_shader_configs.py mame2003 zfast 1920 1080
-python retropie_shader_configs.py 2003plus zfast 1920 1080
-python retropie_shader_configs.py mame2010 zfast 1920 1080
-python retropie_shader_configs.py mame2015 zfast 1920 1080
-python retropie_shader_configs.py mame2016 zfast 1920 1080
-python retropie_shader_configs.py fba2012 zfast 1920 1080
-python retropie_shader_configs.py fbneo zfast 1920 1080
-python retropie_shader_configs.py consoles zfast 1920 1080
+python retropie_shader_configs.py mame2000 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2003 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py 2003plus -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2010 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2015 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py mame2016 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py fba2012 -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py fbneo -s zfast -c false -x 1920 -y 1080 -o all
+python retropie_shader_configs.py consoles -s zfast -c false -x 1920 -y 1080 -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('zfast',False,1920,1080)"
 
-python retropie_shader_configs.py mame2000 zfast 1280 720
-python retropie_shader_configs.py mame2003 zfast 1280 720
-python retropie_shader_configs.py 2003plus zfast 1280 720
-python retropie_shader_configs.py mame2010 zfast 1280 720
-python retropie_shader_configs.py mame2015 zfast 1280 720
-python retropie_shader_configs.py mame2016 zfast 1280 720
-python retropie_shader_configs.py fba2012 zfast 1280 720
-python retropie_shader_configs.py fbneo zfast 1280 720
-python retropie_shader_configs.py consoles zfast 1280 720
+python retropie_shader_configs.py mame2000 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2003 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py 2003plus -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2010 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2015 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py mame2016 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py fba2012 -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py fbneo -s zfast -c false -x 1280 -y 720 -o all
+python retropie_shader_configs.py consoles -s zfast -c false -x 1280 -y 720 -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('zfast',False,1280,720)"
 
-python retropie_shader_configs.py mame2000 zfast curvature
-python retropie_shader_configs.py mame2003 zfast curvature
-python retropie_shader_configs.py 2003plus zfast curvature
-python retropie_shader_configs.py mame2010 zfast curvature
-python retropie_shader_configs.py mame2015 zfast curvature
-python retropie_shader_configs.py mame2016 zfast curvature
-python retropie_shader_configs.py fba2012 zfast curvature
-python retropie_shader_configs.py fbneo zfast curvature
+python retropie_shader_configs.py mame2000 -s zfast -c true -o all
+python retropie_shader_configs.py mame2003 -s zfast -c true -o all
+python retropie_shader_configs.py 2003plus -s zfast -c true -o all
+python retropie_shader_configs.py mame2010 -s zfast -c true -o all
+python retropie_shader_configs.py mame2015 -s zfast -c true -o all
+python retropie_shader_configs.py mame2016 -s zfast -c true -o all
+python retropie_shader_configs.py fba2012 -s zfast -c true -o all
+python retropie_shader_configs.py fbneo -s zfast -c true -o all
 python -c "import retropie_shader_configs; retropie_shader_configs.createZip('zfast',True)"
 
 pause
